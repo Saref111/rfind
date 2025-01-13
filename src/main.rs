3b6c1be70fd8fc258f90f6e31b53a8c9434d6de0
Ivan Borovyk <saref012@gmail.com>
2025-01-13 10:30:06 +0200

    small refctor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs, path::PathBuf, str::FromStr};
+use std::{error::Error, fs::{self, ReadDir}, path::{Path, PathBuf}, str::FromStr};
 
 use clap::Parser;
 
@@ -26,7 +26,7 @@ fn main() {
     }
 }
 
-fn find_files(paths: &Vec<PathBuf>, search_name: &str, is_case_insens: bool) -> Result<(), Box<dyn Error>>{
+fn find_files(paths: &[PathBuf], search_name: &str, is_case_insens: bool) -> Result<(), Box<dyn Error>>{
     if paths.is_empty() {return Ok(());}
     let search_exp = if is_case_insens { search_name.to_lowercase() } else { search_name.to_string() };
 
@@ -36,16 +36,7 @@ fn find_files(paths: &Vec<PathBuf>, search_name: &str, is_case_insens: bool) -> 
         if path.is_dir() {
             match fs::read_dir(&path) {
                 Ok(entries) => {
-                    for entry in entries {
-                        let entry = entry?;
-                        let entry_path = entry.path();
-
-                        if entry_path.is_dir() {
-                            dirs_to_explore.push(entry_path);
-                        } else if is_matching(&entry_path, &search_exp, is_case_insens) {
-                            println!("{}", path.display());
-                        }
-                    }
+                    handle_dir_entries(entries, &mut dirs_to_explore, &search_exp, is_case_insens)?
                 },
                 Err(e) => {
                     eprintln!("Cannot read dir {}: {}", path.display(), e);
@@ -59,7 +50,7 @@ fn find_files(paths: &Vec<PathBuf>, search_name: &str, is_case_insens: bool) -> 
     Ok(())
 }
 
-fn is_matching(path: &PathBuf, search_exp: &str, is_case_insens: bool) -> bool {
+fn is_matching(path: &Path, search_exp: &str, is_case_insens: bool) -> bool {
     path.file_name().and_then(|name| name.to_str()).map(|name| {
         if is_case_insens {
             name.to_lowercase().contains(search_exp)
@@ -67,4 +58,19 @@ fn is_matching(path: &PathBuf, search_exp: &str, is_case_insens: bool) -> bool {
             name.contains(search_exp)
         }
     }).unwrap_or(false)
+}
+
+fn handle_dir_entries(entries: ReadDir, dirs_to_explore: &mut Vec<PathBuf>, search_exp: &str, is_case_insens: bool) -> Result<(), Box<dyn Error>> {
+    for entry in entries {
+        let entry = entry?;
+        let entry_path = entry.path();
+
+        if entry_path.is_dir() {
+            dirs_to_explore.push(entry_path);
+        } else if is_matching(&entry_path, search_exp, is_case_insens) {
+            println!("{}", entry_path.display());
+        }
+    }
+
+    Ok(())
 }
